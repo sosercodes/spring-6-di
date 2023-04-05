0731de2afa1fa9460f9f02fe5d6b414b71ba892c
Simon Oser <simon.oser@gmail.com>
2023-04-05 13:38:33 +0200

    first commit

---
+++ src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
@@ -0,0 +1,14 @@
+package guru.springframework.spring6di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6DiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6DiApplication.class, args);
+		System.out.println("Hello Dependency Injection!");
+	}
+
+}
