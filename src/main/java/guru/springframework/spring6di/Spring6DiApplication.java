8c343f4f8a50f6ed8725cc54abad3241e28e6f07
Simon Oser <simon.oser@gmail.com>
2023-04-05 16:05:27 +0200

    30. The Spring Context

--- src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
+++ src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
@@ -1,14 +1,19 @@
 package guru.springframework.spring6di;
 
+import guru.springframework.spring6di.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Spring6DiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring6DiApplication.class, args);
-		System.out.println("Hello Dependency Injection!");
+		ConfigurableApplicationContext ctx = SpringApplication.run(Spring6DiApplication.class, args);
+		System.out.println("Hello from main!");
+		// MyController myController = ctx.getBean(MyController.class);
+		MyController myController = (MyController) ctx.getBean("myController");
+		System.out.println(myController.sayHello());
 	}
 
 }
---
+++ src/main/java/guru/springframework/spring6di/controllers/MyController.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring6di.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello() {
+        System.out.println("I'm in MyController!");
+        return "Hello from MyController!";
+    }
+}
